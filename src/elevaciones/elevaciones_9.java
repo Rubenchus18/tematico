af1a022b7b1779aa2eeefbc7875123cb41156205
Rubenchus18 <rubengarci327@gmail.com>
2024-05-26 19:32:36 +0200

    cszxczx

--- src/elevaciones/elevaciones_9.java
+++ src/elevaciones/elevaciones_9.java
@@ -3,6 +3,7 @@ package elevaciones;
 public class elevaciones_9 {
 	public static void mains(String[]args) {
 		 System.out.println("hola buenos dia");
+		 System.out.println("hola buenos dia");
 	}
 
 }
